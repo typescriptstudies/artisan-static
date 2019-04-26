de26df42480367197e1689de0ac7a23d226601a5
typescriptstudies <typescriptstudies@gmail.com>
2019-04-26 19:26:00 +0200

    Welcome message

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,11 +1,5 @@
 @extends('_layouts.master')
 
 @section('content')
-    <h1>Welcome to <b>lishadowapps's</b> netlify blog template</h1>
-
-    <h2>Asset Compilation Test</h2>
-
-    <p>CSS test: <span class="test-css">text with border and padding</span></p>
-
-    <p>JavaScript test: <a href="#" class="test-js">click me</a></p>
+    <h1>Welcome to <b>lishadowapps's</b> blog</h1>>
 @endsection
