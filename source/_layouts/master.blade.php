69ff00cfa1ef2300b13d94055a0d6efdca8102e9
typescriptstudies <typescriptstudies@gmail.com>
2019-04-26 20:47:12 +0200

    Styling

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -19,7 +19,7 @@
 <body>
     <header>
         <nav>
-            <strong>{{ $page->site->title }}</strong><br>
+            <strong><span class="maintitle">{{ $page->site->title }}</span></strong><br>
             <ul>
                 <li><a href="/">Home</a></li>
                 <li><a href="/posts">Posts</a></li>
