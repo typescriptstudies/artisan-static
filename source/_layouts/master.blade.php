3576596ae156ac38e27c3d52835356880852155a
typescriptstudies <typescriptstudies@gmail.com>
2019-04-26 20:01:55 +0200

    App css

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -14,8 +14,7 @@
     @include('_partials.head.meta')
     @include('_partials.cms.identity_widget')
 
-    <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
-    <link rel="stylesheet" href="{{ mix('css/app.css', 'assets/build') }}">
+    <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">    
 </head>
 <body>
     <header>
